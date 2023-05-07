7fe276cc6cf70aab2fed0708a1fc3f60d3201928
"kriskoribsky" <kiko.koribsky@gmail.com>
2023-05-07 22:50:32 +0200

    feat: implement item module

---
+++ item.c
@@ -0,0 +1,60 @@
+#define _POSIX_C_SOURCE 200809L
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "item.h"
+
+#ifdef DEBUG
+#include <assert.h>
+#define ASSERT(expr) assert(expr)
+#else
+#define ASSERT(expr) ((void)0)
+#endif
+
+#define MALLOC(nmemb, ptr)                          \
+    {                                               \
+        (ptr) = malloc((nmemb) * (sizeof(*(ptr)))); \
+        assert((ptr) != NULL);                      \
+    }
+
+#define FREE(ptr)     \
+    {                 \
+        free(ptr);    \
+        (ptr) = NULL; \
+    }
+
+#define CHECK_NULL(ptr)    \
+    {                      \
+        if ((ptr) == NULL) \
+        {                  \
+            return NULL;   \
+        }                  \
+    }
+
+struct item *create_item(char *name, char *description, unsigned int properties)
+{
+    CHECK_NULL(name);
+    CHECK_NULL(description);
+
+    struct item *new;
+    MALLOC(1, new);
+
+    new->name = strdup(name);
+    new->description = strdup(description);
+    new->properties = properties;
+
+    ASSERT(new->name != NULL &&new->description != NULL);
+    return new;
+}
+
+struct item *destroy_item(struct item *item)
+{
+    CHECK_NULL(item);
+
+    FREE(item->name);
+    FREE(item->description);
+    FREE(item);
+
+    return NULL;
+}
