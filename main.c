6071314413a681f8cd6fea0a29cb25fb1721a8b4
"kriskoribsky" <kiko.koribsky@gmail.com>
2023-05-16 10:27:22 +0200

    fix: restore old main

--- main.c
+++ main.c
@@ -7,35 +7,28 @@
 
 int main(void)
 {
-    // struct game *game = create_game();
+    struct game *game = create_game();
 
-    // play_game(game);
+    play_game(game);
 
-    // switch (game->state)
-    // {
-    // case SOLVED:
-    //     printf("Contratulations, you won!\n");
-    //     break;
+    switch (game->state)
+    {
+    case SOLVED:
+        printf("Contratulations, you won!\n");
+        break;
 
-    // case GAMEOVER:
-    //     break;
+    case GAMEOVER:
+        break;
 
-    // case RESTART:
-    //     destroy_game(game);
-    //     return main();
+    case RESTART:
+        destroy_game(game);
+        return main();
 
-    // case PLAYING:
-    //     fprintf(stderr, "Error! Game exited prematurely.\n");
-    //     exit(EXIT_FAILURE);
-    // }
+    case PLAYING:
+        fprintf(stderr, "Error! Game exited prematurely.\n");
+        exit(EXIT_FAILURE);
+    }
 
-    // destroy_game(game);
-    // return EXIT_SUCCESS;
-
-    char *name = "PRESKUMAJ";
-    char *description = "Preskuma zvoleny predmet.";
-    struct command *command = create_command(name, description, NULL, 0);
-
-    printf("comparison result: %d", strcasecmp(name, command->name) && strcasecmp(description, command->description));
-
+    destroy_game(game);
+    return EXIT_SUCCESS;
 }
